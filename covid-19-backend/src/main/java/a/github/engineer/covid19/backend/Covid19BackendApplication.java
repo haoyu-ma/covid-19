e26ea649328318d8a3f2bed46b4cb27e534c7113
marhoury <marhoury@hotmail.com>
2020-04-10 11:02:22 +1200

    Initial COVID-19 Backend

---
+++ covid-19-backend/src/main/java/a/github/engineer/covid19/backend/Covid19BackendApplication.java
@@ -0,0 +1,13 @@
+package a.github.engineer.covid19.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19BackendApplication.class, args);
+	}
+
+}
---
+++ covid-19-backend/src/test/java/a/github/engineer/covid19/backend/Covid19BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package a.github.engineer.covid19.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
